ca742259aa8b8c65c3f1006edf0ea3793926e664
UNNAMED <106890463+AMBYSBOTZ@users.noreply.github.com>
2022-11-23 17:32:33 +0700

    index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('Bot Whatsapp MD', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
-say(`'Yushino-Md-V3' By AL? Offc`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
+say('*X I A O - S A N*', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
+say(`'AMBHS-BOTZ-V3' By XIAO SAN`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
 
 var isRunning = false
 /**
