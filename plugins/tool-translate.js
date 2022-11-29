f0f7806b249b52ba6eda0dda8c5c007333f2f41b
UNNAMED <106890463+AMBYSBOTZ@users.noreply.github.com>
2022-11-29 23:47:54 +0700

    tool-translate.js

--- plugins/tool-translate.js
+++ plugins/tool-translate.js
@@ -1,18 +1,35 @@
-import translate from '@vitalets/google-translate-api'
+const { translate } = require('@vitalets/google-translate-api');
+const defaultLang = 'es'
+const tld = 'cn'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-	let lang, text
-	if (args.length >= 2) {
-		lang = args[0] ? args[0] : 'id', text = args.slice(1).join(' ')
-	} else if (m.quoted && m.quoted.text) {
-		lang = args[0] ? args[0] : 'id', text = m.quoted.text
-	} else throw `Ex: ${usedPrefix + command} id hello i am robot`
-	let res = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null)
-	if (!res) throw `Error : Bahasa"${lang}" Tidak Support`
-	m.reply(`*Terdeteksi Bahasa:* ${res.from.language.iso}\n*Ke Bahasa:* ${lang}\n\n*Terjemahan:* ${res.text}`.trim())
-}
-handler.help = ['translate'].map(v => v + ' <bahasa> <teks>')
-handler.tags = ['tools']
-handler.command = /^(tr(anslate)?)$/i
+    let err = `
+📌 *Example:*
 
-export default handler
+*${usedPrefix + command}* <lang> [texto]
+*${usedPrefix + command}* id Hello World
+
+≡ *Lista de idiomas admitidos:* 
+
+https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+   
+       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
+       m.reply(result.text)
+
+
+}
+handler.help = ['trad <leng> <text>']
+handler.tags = ['tools']
+handler.command = ['translate', 'tl', 'trad', 'tr']
+
+module.exports = handler
